df85bb2c6b311d9fd4b79715bbb55ff9818be1d0
Luca Succi <luca.succi4@studio.unibo.it>
2022-03-11 10:16:51 +0100

    OTP pkg version selection is now strict

--- src/grisp_tools_step.erl
+++ src/grisp_tools_step.erl
@@ -142,20 +142,16 @@ parse_version([Build, Extra1, Extra2, Major, Minor, Patch, Pre, Full]) ->
 parse_version_integer(<<>>) -> <<>>;
 parse_version_integer(Version) -> binary_to_integer(Version).
 
-find_version({[], _, _, _}, []) ->
-    {error, not_found};
-find_version({[], _, _, _}, Versions) ->
-    {ok, hd(lists:reverse(lists:sort(Versions)))};
-find_version({[{N, V}|Version], Pre, Build, Full}, Versions) ->
-    find_version(
-        {Version, Pre, Build, Full},
-        lists:filter(fun
-            ({Ver, P, B, _F}) when P =:= Pre, B =:= Build ->
-                length(Ver) >= N andalso lists:nth(N, Ver) == V;
+find_version({_VN_list, Pre, Build, Full}, Versions) ->
+    case lists:filter(fun
+            ({_Ver, P, B, VerFullBin}) 
+              when P =:= Pre, B =:= Build, VerFullBin =:= Full ->
+                true;
             (_) ->
-                false
-        end, Versions)
-    ).
+                false end, Versions) of
+        [V|_] -> {ok, V};
+        [] -> {error, not_found}
+    end.
 
 version_list({Version, Pre, Build, _Full}, N, [Last|_] = List) when
     N > length(Version)
